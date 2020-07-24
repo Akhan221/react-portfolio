ac1cb776464a686f867f7f570c973db567318166
Ahmad Khan <aak43@duke.edu>
2020-07-24 10:49:30 -0500

    implemented modal in projects section

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Modal, Image, Header } from 'semantic-ui-react';
 
 class Portfolio extends Component {
   render() {
@@ -8,7 +9,8 @@ class Portfolio extends Component {
         var projectImage = 'images/portfolio/'+projects.image;
         return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
-            <a href={projects.url} target="_blank" title={projects.title}>
+
+            <Modal trigger={<a href="javascript:void(0)" title={projects.title}>
                <img alt={projects.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
@@ -17,7 +19,16 @@ class Portfolio extends Component {
                   </div>
                 </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
+            </a>}>
+      <Modal.Header>{projects.title}</Modal.Header>
+    <Modal.Content image>
+      <Image wrapped size='medium' src={projectImage} />
+      <Modal.Description>
+      <Header>{projects.title}</Header>
+      <p>{projects.category}</p>
+      </Modal.Description>
+    </Modal.Content>
+  </Modal>
           </div>
         </div>
       })
@@ -34,6 +45,7 @@ class Portfolio extends Component {
 
             <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
                 {projects}
+
             </div>
           </div>
       </div>
